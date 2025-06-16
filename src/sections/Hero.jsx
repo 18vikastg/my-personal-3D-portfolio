c7d60284640b5126af5bc49e0e1f7ed9b983800e
18vikastg <vikastg2000@gmail.com>
2025-06-16 13:34:42 +0530

    added things

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { words } from '../constants'
+import Button from '../components/Button'
 
 
 
@@ -38,6 +39,15 @@ const Hero = () => {
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
+              <p className='text-white-50 md:text-xl relative z-10 
+                  pointer-events-none'>
+                    Hi, I'm Vikas T G, a developer based in India with a passion for code. 
+                  </p>
+                  <Button 
+                  className='md:w-80 md:h-16 w-60 h-12'
+                  id='button'
+                  text='See my work'
+                  />
           </div>
         </header>
         {/*RIGHT: 3D MODEL*/}
