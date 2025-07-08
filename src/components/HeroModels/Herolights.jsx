f302be31ed008781d7e91325d31c38d825f423cc
18vikastg <vikastg2000@gmail.com>
2025-07-08 12:51:21 +0530

    chnaged the model

--- src/components/HeroModels/Herolights.jsx
+++ src/components/HeroModels/Herolights.jsx
@@ -2,40 +2,75 @@ import * as THREE from "three";
 
 const HeroLights = () => (
   <>
-    {/* Strong Red from left */}
+    {/* Vibrant hot pink from left - main key light */}
     <spotLight
-      position={[-5, 5, 5]}
-      angle={0.5}
-      penumbra={1}
-      intensity={80}
-      color="#ff005b"
+      position={[-6, 6, 6]}
+      angle={0.6}
+      penumbra={0.8}
+      intensity={120}
+      color="#ff1493"
     />
-    {/* Strong Green from right */}
+
+    {/* Electric cyan from right - secondary key */}
     <spotLight
-      position={[5, 5, 5]}
-      angle={0.5}
-      penumbra={1}
-      intensity={80}
-      color="#00ffae"
+      position={[6, 6, 6]}
+      angle={0.6}
+      penumbra={0.8}
+      intensity={110}
+      color="#00ffff"
     />
-    {/* Strong Blue from above/back */}
+
+    {/* Neon purple from above/back - rim light */}
     <spotLight
-      position={[0, 8, -5]}
+      position={[0, 10, -6]}
+      angle={0.8}
+      penumbra={0.9}
+      intensity={90}
+      color="#9d4edd"
+    />
+
+    {/* Toxic green accent from below-left */}
+    <spotLight
+      position={[-4, -2, 8]}
       angle={0.7}
       penumbra={1}
-      intensity={70}
-      color="#0099ff"
+      intensity={60}
+      color="#39ff14"
     />
-    {/* Soft purple/pink fill from front */}
+
+    {/* Orange gaming light from below-right */}
     <spotLight
-      position={[0, 2, 10]}
-      angle={0.8}
+      position={[4, -2, 8]}
+      angle={0.7}
       penumbra={1}
-      intensity={40}
-      color="#a259ff"
+      intensity={50}
+      color="#ff6b35"
     />
-    {/* Optional: subtle white fill */}
-    <ambientLight intensity={0.15} color="#ffffff" />
+
+    {/* Bright blue fill from front */}
+    <spotLight
+      position={[0, 3, 12]}
+      angle={1.0}
+      penumbra={1}
+      intensity={70}
+      color="#0066ff"
+    />
+
+    {/* Extra RGB chaos - moving light positions */}
+    <pointLight
+      position={[3, 4, 2]}
+      intensity={40}
+      color="#ff00ff"
+    />
+
+    <pointLight
+      position={[-3, 4, 2]}
+      intensity={40}
+      color="#00ff00"
+    />
+
+    {/* Subtle warm ambient to balance the chaos */}
+    <ambientLight intensity={0.25} color="#fdf2f8" />
   </>
 );
 
